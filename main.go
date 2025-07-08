f51df9979837b02006c172b98e04efee1858d6b0
Fuzari <andreiiakovlev@MacBook-Pro-Andrei.local>
2025-07-08 23:39:01 +0300

    Implemented two servers

--- main.go
+++ main.go
@@ -1,33 +1,74 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
-	"os"
 )
+
+const keyServerAddr MyString = "serverAddr"
+
+type MyString string
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe("127.0.0.1:3333", mux)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("Server connection was closed.\n")
-	} else {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	serverOne := &http.Server{
+		Addr:    "127.0.0.1:3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
 	}
+
+	serverTwo := &http.Server{
+		Addr:    "127.0.0.1:4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Got / request\n")
+	context := r.Context()
+	fmt.Printf("%s got / request\n", context.Value(keyServerAddr))
 	io.WriteString(w, "This is my website!\n")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Got /hello request\n")
+	context := r.Context()
+	fmt.Printf("%s got /hello request\n", context.Value(keyServerAddr))
 	io.WriteString(w, "Hello, Golang!\n")
 }
