44e6ed469023fd810e8b3cdd153bb8ef326f08dd
Fuzari <andreiiakovlev@MacBook-Pro-Andrei.local>
2025-07-08 22:49:51 +0300

    Created initial server impl

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe("127.0.0.1:3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server connection was closed.\n")
+	} else {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Got /hello request\n")
+	io.WriteString(w, "Hello, Golang!\n")
+}
