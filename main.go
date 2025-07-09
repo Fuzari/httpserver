b54deb91318b8a52af418da70d8c625a4f668436
Fuzari <andreiiakovlev@MacBook-Pro-Andrei.local>
2025-07-09 21:33:22 +0300

    Readed body from / request

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 )
@@ -19,8 +20,9 @@ func main() {
 	mux.HandleFunc("/hello", getHello)
 
 	ctx, cancelCtx := context.WithCancel(context.Background())
+	defer cancelCtx()
 
-	serverOne := &http.Server{
+	server := &http.Server{
 		Addr:    "127.0.0.1:3333",
 		Handler: mux,
 		BaseContext: func(l net.Listener) context.Context {
@@ -29,41 +31,35 @@ func main() {
 		},
 	}
 
-	serverTwo := &http.Server{
-		Addr:    "127.0.0.1:4444",
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-			return ctx
-		},
+	err := server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error listening for server one: %s\n", err)
 	}
-
-	go func() {
-		err := serverOne.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server one closed\n")
-		} else if err != nil {
-			fmt.Printf("error listening for server one: %s\n", err)
-		}
-		cancelCtx()
-	}()
-
-	go func() {
-		err := serverTwo.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server two closed\n")
-		} else if err != nil {
-			fmt.Printf("error listening for server two: %s\n", err)
-		}
-		cancelCtx()
-	}()
-
-	<-ctx.Done()
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	context := r.Context()
-	fmt.Printf("%s got / request\n", context.Value(keyServerAddr))
+
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	bodyData, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("could not read body: %s\n", err)
+	}
+
+	fmt.Printf("%s got / request first(%t)=%s, second(%t)=%s body:\n%s\n",
+		context.Value(keyServerAddr),
+		hasFirst,
+		first,
+		hasSecond,
+		second,
+		bodyData,
+	)
 	io.WriteString(w, "This is my website!\n")
 }
 
